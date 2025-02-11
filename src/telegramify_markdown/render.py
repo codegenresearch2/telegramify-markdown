bd73009335861e661a32a554d2d8b68d7d0863de
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 19:58:40 +0000

    Reset to c4659296b74272b43c20e889f13587c644fc1a62 and made changes for LLM Round 1

--- src/telegramify_markdown/render.py
+++ src/telegramify_markdown/render.py
@@ -4,15 +4,15 @@ from mistletoe import block_token, span_token
 from mistletoe.markdown_renderer import MarkdownRenderer, LinkReferenceDefinition, Fragment
 from telebot import formatting
 
-from .customize import markdown_symbol, strict_markdown
+from .customize import markdown_symbol
 
+strict_markdown = True
 
 class TelegramMarkdownRenderer(MarkdownRenderer):
 
     def render_heading(
             self, token: block_token.Heading, max_line_length: int
     ) -> Iterable[str]:
-        # note: no word wrapping, because atx headings always fit on a single line.
         line = ""
         if token.level == 1:
             line += markdown_symbol.head_level_1
@@ -41,12 +41,6 @@ class TelegramMarkdownRenderer(MarkdownRenderer):
         yield indentation + token.delimiter
 
     def render_inline_code(self, token: span_token.InlineCode) -> Iterable[Fragment]:
-        if len(token.delimiter) == 3:
-            return self.embed_span(
-                Fragment(token.delimiter + token.padding + "\n"),
-                token.children,
-                Fragment(token.padding + token.delimiter)
-            )
         return self.embed_span(
             Fragment(token.delimiter + token.padding),
             token.children,
@@ -68,16 +62,9 @@ class TelegramMarkdownRenderer(MarkdownRenderer):
         return super().render_emphasis(token)
 
     def render_strong(self, token: span_token.Strong) -> Iterable[Fragment]:
-        if strict_markdown:
-            # Telegram strong: *text*
-            # Markdown strong: **text** or __text__
-            return self.embed_span(Fragment('*'), token.children)
-        else:
-            # bold
-            if token.delimiter == "*":
-                return self.embed_span(Fragment(token.delimiter * 1), token.children)
-            # underline
-            return self.embed_span(Fragment(token.delimiter * 2), token.children)
+        if token.delimiter == "*":
+            return self.embed_span(Fragment(token.delimiter * 1), token.children)
+        return self.embed_span(Fragment(token.delimiter * 2), token.children)
 
     def render_strikethrough(
             self, token: span_token.Strikethrough
@@ -117,23 +104,15 @@ class TelegramMarkdownRenderer(MarkdownRenderer):
     ) -> Iterable[Fragment]:
         title = next(self.span_to_lines(token.children, max_line_length=20), "")
         if token.dest_type == "uri" or token.dest_type == "angle_uri":
-            # "[" description "](" dest_part [" " title] ")"
-            # "[" description "](" dest_part [" " title] ")"
-            yield Fragment(formatting.mlink(url=target, content=title, escape=True)
-                           )
+            yield Fragment(formatting.mlink(url=target, content=title, escape=True))
         elif token.dest_type == "full":
-            # "[" description "][" label "]"
             yield from (
                 Fragment(formatting.escape_markdown("[")),
                 Fragment(token.label, wordwrap=True),
                 Fragment(formatting.escape_markdown("]")),
             )
         elif token.dest_type == "collapsed":
-            # "[" description "][]"
-            yield Fragment(formatting.escape_markdown("[]")),
-        else:
-            # "[" description "]"
-            pass
+            yield Fragment(formatting.escape_markdown("[]"))
 
     def render_auto_link(self, token: span_token.AutoLink) -> Iterable[Fragment]:
         yield Fragment(formatting.escape_markdown("<") + token.children[0].content + formatting.escape_markdown(">"))
@@ -141,13 +120,10 @@ class TelegramMarkdownRenderer(MarkdownRenderer):
     def render_escape_sequence(
             self, token: span_token.EscapeSequence
     ) -> Iterable[Fragment]:
-        # 渲染转义字符
-        # because the escape_markdown already happened in the parser, we can skip it here.
         yield Fragment("" + token.children[0].content)
 
     def render_table(
             self, token: block_token.Table, max_line_length: int
     ) -> Iterable[str]:
-        # note: column widths are not preserved; they are automatically adjusted to fit the contents.
         fs = super().render_table(token, max_line_length)
         return [formatting.mcode("\n".join(fs))]

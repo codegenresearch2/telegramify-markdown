c2fb83be961bead343ab2e22afd8311a0f0f85dd
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 19:46:34 +0000

    Reset to 24535f3bdc89557bfad47d1bfae5e27231d5af5a and made changes for LLM Round 1

--- playground/use_case.py
+++ playground/use_case.py
@@ -4,8 +4,26 @@ from telegramify_markdown.customize import markdown_symbol
 markdown_symbol.head_level_1 = "📌"  # If you want, Customizing the head level 1 symbol
 markdown_symbol.link = "🔗"  # If you want, Customizing the link symbol
 md = """
-*bold _italic bold ~italic bold strikethrough ||italic bold strikethrough spoiler||~ __underline italic bold___ bold*
-~strikethrough~
+---
+key: value
+---
+
+\(c!ode\)
+\# Heading Level 1 `c!ode`
+# Heading Level 1 `c!ode`
+## Heading Level 2
+### Heading Level 3
+**Bold text**
+*Italic text*
+~~Strikethrough text~~
+> Blockquote text
+`Inline code`
+\\/\\111`sad`
+
+
+Code block
+
+
 """
 converted = telegramify_markdown.convert(md)
 print(converted)

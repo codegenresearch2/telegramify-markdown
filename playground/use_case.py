69139f5cf957b95ff82d68f1ce40ed445ab43e7a
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 21:48:31 +0000

    Reset to 24535f3bdc89557bfad47d1bfae5e27231d5af5a and made changes for LLM Round 1

--- playground/use_case.py
+++ playground/use_case.py
@@ -2,6 +2,9 @@ import os
 import telegramify_markdown
 from telegramify_markdown.customize import markdown_symbol
 
+# Set the environment variable for the Telegram bot token
+os.environ['TELEGRAM_BOT_TOKEN'] = os.environ.get('TELEGRAM_BOT_TOKEN', 'your_default_token_here')
+
 # Customizing the head level 1 symbol
 markdown_symbol.head_level_1 = "📌"
 # Customizing the link symbol

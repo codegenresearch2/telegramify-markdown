0eb3c50efac5d91aaec5471246b4dc83510321e1
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 12:41:50 +0000

    Reset to c4659296b74272b43c20e889f13587c644fc1a62 and made changes for LLM Round 1

--- src/telegramify_markdown/customize.py
+++ src/telegramify_markdown/customize.py
@@ -1,18 +1,11 @@
 import emoji
 
-
-class Symbol(object):
-    head_level_1 = emoji.emojize(":pushpin:")
-    # "📌"
-    head_level_2 = emoji.emojize(":pencil:")
-    # "✏"
-    head_level_3 = emoji.emojize(":books:")
-    # "📚"
-    head_level_4 = emoji.emojize(":bookmark:")
-    # "📒"
+class Symbol:
+    head_level_1 = "📌"
+    head_level_2 = "✏"
+    head_level_3 = "📚"
+    head_level_4 = "📒"
     image = "🖼"
     link = "🔗"
 
-
 markdown_symbol = Symbol()
-strict_markdown = True
